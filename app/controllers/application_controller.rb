13f53da3e1b4a40de88b3668ef3e84bbad41129e
Mladen <blagmash@gmail.com>
2020-06-25 11:13:48 +0200

    last commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,5 @@
 class ApplicationController < ActionController::API
 
-    def index 
-
-
-        rooms= Room.all
-
-        render json: rooms , indlude: "**"
-    end
-
-
-    def create
-         if room= Room.find_by(name: params[:name]) === nil
-            room= Room.create(name: params[:name])
-         else
-            room = Room.find_by(name: params[:name])
-         end
-         ActionCable.server.broadcast 'rooms_channel', room
-    end
+ 
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :rooms, :messages
+  resources  :messages
   mount ActionCable.server => '/cable'
 end
